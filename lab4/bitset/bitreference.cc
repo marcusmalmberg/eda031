2a04438fea406ed14e9166f3e3f7cef668dbaccf
Marcus Malmberg <marcus.j.malmberg@gmail.com>
2012-02-25 23:22:52 +0100

    Solved A4.

--- lab4/bitset/bitreference.cc
+++ lab4/bitset/bitreference.cc
@@ -1,27 +1,31 @@
 #include "bitreference.h"
 #include "bitset.h"
 
+#include <iostream>
+
 namespace cpp_lab4 {
     BitReference& BitReference::operator=(bool x) {
-        //
-        // *** IMPLEMENT ***
-        // Note that this corresponds to the set() function in SimpleBitset.
-        //
+		if(x) {
+			*p_bits |= (1L << pos);
+		} else {
+			*p_bits &= ~(1L << pos);
+		}
         return *this;
     }
     
     BitReference& BitReference::operator=(const BitReference& bsr) {
-        //
-        // *** IMPLEMENT ***
-        //
+	
+		bool x = ((*bsr.p_bits & (1L << bsr.pos)) != 0);
+		if(x) {
+			*p_bits |= (1L << pos);
+		} else {
+			*p_bits &= ~(1L << pos);
+		}
+
         return *this;
     }
     
     BitReference::operator bool() const {
-        //
-        // *** IMPLEMENT ***
-        // Note that this corresponds to the get() function in SimpleBitset.
-        //
-        return false;
+		return (*p_bits & (1L << pos)) != 0;
     }
 }
