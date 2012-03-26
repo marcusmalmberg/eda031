2ec84ee1dc938ac3603b3ab111dd522b3a84005e
Fredrik Appelros <dt07fa4@student.lth.se>
2012-03-26 20:53:36 +0200

    completed article.h

--- project/clientserver/article.h
+++ project/clientserver/article.h
@@ -8,7 +8,14 @@ using namespace std;
 namespace news_server {
 
 	class Article {
-
+	friend ostream& operator<<(ostream& out, const Article& a);
+	public:
+		void read();
+	private:
+		size_t id;
+		string title;
+		string author;
+		string text;
 	}
 
 }
--- project/clientserver/db_interface.h
+++ project/clientserver/db_interface.h
@@ -11,21 +11,14 @@ using namespace std;
 namespace news_server {
 
 	class DBInterface {
-
+	public:
 		virtual vector<Newsgroup> list_newsgroups() = 0;
-
 		virtual vector<Article> list_articles(const Newsgroup& ng) = 0;
-
 		virtual size_t create_newsgroup(const string& name) = 0;
-
 		virtual size_t delete_newsgroup(const Newsgroup& ng) = 0;
-
 		virtual read_article(size_t id) = 0;
-
 		virtual size_t write_article(const Article& a) = 0;
-
 		virtual size_t delete_article(const Article& a) = 0;
-
 	}
 
 }
